6c8f5db9ff2bde1cc5c04aedaf7a9195c5d60f72
Isaac Maier <padspambox@gmail.com>
2020-10-06 18:38:46 -0400

    Add a basic image GET request test

--- test/image.test.js
+++ test/image.test.js
@@ -2,6 +2,19 @@ const { diffbot, expect } = require('./global');
 
 describe('Image Tests', function() {
 
+  it('should generate the image GET request', async () => {
+    const url = 'https://www.deviantart.com/up-tchi/art/Coral-village-852927725';
+
+    const request = await diffbot.image({ url });
+
+    expect(request.url).to.equal(`https://api.diffbot.com/v3/image?token=${process.env.DIFFBOT_API_TOKEN}&url=${encodeURIComponent(url)}`);
+    expect(request.method).to.equal('GET');
+    expect(request.body).to.be.undefined;
+    expect(request.headers).to.be.an('object').that.is.empty;
+
+    return Promise.resolve(true);
+  });
+
   it('should generate the image POST request', async () => {
     const url = 'https://www.google.com/';
     const body = '<html><body><h2>This is the Google logo</h2><div><img src="images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"></div></body></html>';
